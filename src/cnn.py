877547d01d9560379b15ece326080d80496820eb
masaponto <masaponto@gmail.com>
2018-06-09 22:53:41 +0900

    [Fix] use tf.layers

--- src/cnn.py
+++ src/cnn.py
@@ -6,56 +6,67 @@ import cv2
 import glob
 import numpy as np
 import model
+from tensorflow.python.framework import graph_util
 
 
-def main():
-    true_path = './resized/'
+def load_data(true_path, false_path):
     true_files = glob.glob(true_path + '*jpg')
-
-    false_path = './blured/'
     false_files = glob.glob(false_path + '*jpg')
 
     labels = np.append(np.ones(len(true_files)),
                        -1 * np.ones(len(false_files)))
 
     labels = labels.reshape(-1, 1)
-    images = []
 
+    images = []
     for f_in in true_files + false_files:
         img = cv2.imread(f_in)
         img = cv2.resize(img, (28, 28))
         images.append(img.flatten().astype(np.float32) / 255.0)
 
-    images = np.array(images)
+    return np.array(images), labels
 
-    with tf.variable_scope("convolutional"):
-        x = tf.placeholder(tf.float32, shape=[None, 2352])
-        keep_prob = tf.placeholder("float")
-        y_conv, variables = model.convolutional(x, keep_prob)
 
-    y_ = tf.placeholder(tf.float32, shape=[None, 1])
+def main():
+
+    # load data
+    images, labels = load_data(true_path='./resized/', false_path='./blured/')
+
+    # input/output
+    x = tf.placeholder(tf.float32, shape=[None, 2352])
+    y = tf.placeholder(tf.float32, shape=[None, 1])
+
+    # train logits
+    training_logits = model.convolutional(x)
+
+    # loss function
     cross_entropy = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+        tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=training_logits))
+
+    # optimizer
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+
+    correct_prediction = tf.equal(
+        tf.argmax(training_logits, 1), tf.argmax(y, 1))
+
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    saver = tf.train.Saver(variables)
+    saver = tf.train.Saver()
     batch_size = 50
 
     with tf.Session() as sess:
-
         sess.run(tf.global_variables_initializer())
+
         for epoch in range(100):
             for i in range(int(len(images) / batch_size)):
                 batch = images[i: i + batch_size], labels[i: i + batch_size]
 
                 train_step.run(
-                    feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+                    feed_dict={x: batch[0], y: batch[1]})
 
-            if epoch % 100 == 0:
+            if epoch % 10 == 0:
                 train_accuracy = accuracy.eval(feed_dict={
-                    x: batch[0], y_: batch[1], keep_prob: 1.0})
+                    x: batch[0], y: batch[1]})
                 print('step %d, training accuracy %g' %
                       (epoch, train_accuracy))
 
--- src/model.py
+++ src/model.py
@@ -3,47 +3,38 @@
 import tensorflow as tf
 
 
-def convolutional(x, keep_prob):
+def convolutional(x, mode=True):
 
-    def weight_variable(shape):
-        initial = tf.truncated_normal(shape, stddev=0.1)
-        return tf.Variable(initial)
+    input_layer = tf.reshape(x, [-1, 28, 28, 3], name="input")
 
-    def bias_variable(shape):
-        initial = tf.constant(0.1, shape=shape)
-        return tf.Variable(initial)
+    conv1 = tf.layers.conv2d(
+        inputs=input_layer,
+        filters=32,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu,
+        name="conv1")
 
-    def conv2d(x, W):
-        return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
 
-    def max_pool_2x2(x):
-        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    conv2 = tf.layers.conv2d(
+        inputs=pool1,
+        filters=64,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu,
+        name="conv2")
 
-    W_conv1 = weight_variable([5, 5, 3, 32])
-    b_conv1 = bias_variable([32])
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
 
-    x_image = tf.reshape(x, [-1, 28, 28, 3])
+    pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
 
-    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-    h_pool1 = max_pool_2x2(h_conv1)
+    dense = tf.layers.dense(
+        inputs=pool2_flat, units=1024, activation=tf.nn.relu)
 
-    W_conv2 = weight_variable([5, 5, 32, 64])
-    b_conv2 = bias_variable([64])
+    dropout = tf.layers.dropout(
+        inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
 
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-    h_pool2 = max_pool_2x2(h_conv2)
+    output = tf.layers.dense(inputs=dropout, units=1)
 
-    W_fc1 = weight_variable([7 * 7 * 64, 1024])
-    b_fc1 = bias_variable([1024])
-
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
-    W_fc2 = weight_variable([1024, 1])
-    b_fc2 = bias_variable([1])
-
-    y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
-
-    return y_conv, (W_conv1, b_conv1, W_conv2, b_conv2, W_fc1, b_fc1, W_fc2, b_fc2)
+    return tf.identity(output, name="inference")
