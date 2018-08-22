0279624670296b29cf33d68fde80e8780e330a8e
masaponto <masaponto@gmail.com>
2018-08-22 23:09:35 +0900

    [Add] gamma_correction for generating dark image

--- src/image_converter.py
+++ src/image_converter.py
@@ -70,11 +70,38 @@ class ImageConverter:
             cmd = f"convert {f_in} -motion-blur {angle}x{radius}+{angle} {f_out}"
             subprocess.call(cmd, shell=True)
 
+    @staticmethod
+    def gamma_correction(input_dir: str, output_dir: str):
+        import cv2
+        import random
+
+        if output_dir == input_dir:
+            raise Exception("Input directory and output directory are same.")
+
+        p = Path(input_dir)
+        files = p.glob('*.jpg')
+        os.makedirs(output_dir, exist_ok=True)
+
+        lookup_table = np.zeros((256, 1), dtype='uint8')
+
+        for f_in in files:
+
+            gamma = random.uniform(0.1, 0.2)
+            for i in range(256):
+                lookup_table[i][0] = 255 * pow(float(i) / 255, 1.0 / gamma)
+
+            img = cv2.imread(str(f_in))
+            img_gamma = cv2.LUT(img, lookup_table)
+            base_name = os.path.basename(f_in)
+            f_out = f"{output_dir}/{base_name}"
+            cv2.imwrite(f_out, img_gamma)
+
 
 def main():
     converter = ImageConverter()
-    converter.resize('../image_data/beautiful/food/', './fuga')
-    converter.motion_bluer('./resized/', './hoge')
+    #converter.resize('../image_data/beautiful/food/', './fuga')
+    #converter.motion_bluer('./resized/', './hoge')
+    converter.gamma_correction('./resized/', './dark')
 
 
 if __name__ == "__main__":
